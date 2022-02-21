caa64823fe9429f065a2d617425f3425c4151e68
missxa <aliona.kharchenko@gmail.com>
2022-02-21 15:21:37 +0100

    waiting to reach the target joint pos before sending a new one

--- scripts/sinusoid.py
+++ scripts/sinusoid.py
@@ -25,7 +25,7 @@ req.config.setpoint = [0]*len(ids)
 srv(req)
 
 pub = rospy.Publisher('/roboy/oxford/middleware/MotorCommand', MotorCommand, queue_size=1)
-factor = 1160000
+factor = 216000
 # factor = 6000
 # factor = 0.01
 setpoints = (2*factor*np.sin(np.pi*t)) #+ 216000# + 1)*np.mean(r)
@@ -36,7 +36,7 @@ cmd = MotorCommand()
 ids = [0] #[0,2,3,4,5,6,7]
 cmd.global_id = ids #[0,1,3,4,5,6,7]
 
-rate = rospy.Rate(1)
+rate = rospy.Rate(100)
 
 for id in ids:
 
@@ -46,7 +46,7 @@ for id in ids:
     while not rospy.is_shutdown() and 10000>i:
     # for s in setpoints:
         # if i==0: i += 1;continue
-        cmd.setpoint = [setpoints[i+6]]*len(ids)
+        cmd.setpoint = [setpoints[i]]*len(ids)
         print(i)
         # print(setpoints[i])
         i += 1
